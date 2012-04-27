1b477b0487889b43b3775d5b821319b1e41a5a7d
Nick Booker <NMBooker@gmail.com>
2012-04-27 16:33:03 +0100

    Add original contact name to lead display sidebar.

--- lib/ffcrm_direct_marketing_helpers.rb
+++ lib/ffcrm_direct_marketing_helpers.rb
@@ -1,4 +1,9 @@
 require "ffcrm_direct_marketing_helpers/engine"
 
 module FfcrmDirectMarketingHelpers
+  class LeadCallback < FatFreeCRM::Callback::Base
+    def show_lead_sidebar_bottom(view, context={})
+      "<li><tt>Original Contact:</tt><dt>#{context[:lead].original_contact}</dt></li>"
+    end
+  end
 end
